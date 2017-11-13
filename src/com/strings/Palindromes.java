d44fbcaa8b26745d84122bc676681e889a3097f8
Piyush Ravi <mails4ipiyush@gmail.com>
2017-11-13 23:56:07 +0530

    strin problems

---
+++ src/com/strings/Palindromes.java
@@ -0,0 +1,33 @@
+package com.strings;
+
+public class Palindromes {
+	
+	public static boolean palindromePermutation(String input) {
+		
+		// assuming ascii characters : total chars 128
+		// 1. build char freq map
+		int [] freqMap = new int[128];
+		for(int i = 0; i < input.length(); i++) {
+			if(input.charAt(i) != ' ') {
+				freqMap[input.charAt(i)]++; 
+			}
+		}
+		
+		//2. numOdd > 1 means it cannot form palindrome
+		int numOdd = 0;
+		for(int i = 0; i < freqMap.length; i++) {
+			if(freqMap[i] % 2 != 0) {
+				numOdd++;
+			}
+		}
+		
+		return numOdd > 1 ? false : true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(palindromePermutation("aab"));
+		System.out.println(palindromePermutation("aabcc bb"));
+		System.out.println(palindromePermutation("taco cat"));
+		System.out.println(palindromePermutation("aab aaa"));
+	}
+}
---
+++ src/com/strings/UniqueString.java
@@ -0,0 +1,6 @@
+package com.strings;
+
+public class UniqueString {
+
+	
+}
