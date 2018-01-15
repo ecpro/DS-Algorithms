8f11af68d2672f516c50390eb2d5bab167c46498
Piyush Ravi <mails4ipiyush@gmail.com>
2018-01-15 22:32:12 +0530

    improve linked list implementation

--- src/com/generics/LinkedList.java
+++ src/com/generics/LinkedList.java
@@ -4,79 +4,85 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LinkedList {
-	
-	private Node head = new Node(Integer.MIN_VALUE, null);
+
+	private Node head;
 	private int count = 0;
 	private int total = 0;
 	private int target = 0;
 	private boolean isPalindrome = true;
-	
+
 	public void add(int key) {
-		if(head.getNext() == null) {
-			head.setNext(new Node(key, null));
+		Node node = new Node(key, null);
+		if (head == null) {
+			head = node;
 			return;
-		}
-		else {
+		} else {
 			Node temp = head;
-			while(temp.getNext() != null) {
+			while (temp.getNext() != null) {
 				temp = temp.getNext();
 			}
 			temp.setNext(new Node(key, null));
 		}
+
+	}
+
+	public void removeNext(Node node) {
+		if (node != null) {
+			node.setNext(node.getNext().getNext());
+			node.getNext().setNext(null);
+		}
+	}
+
+	public void remove(int key) {
+		if (this.head == null)
+			throw new NullPointerException("Cannot delete from empty list");
+		if (this.head.getKey() == key) {
+			this.head = head.getNext();
+			return;
+		}
+		Node curr = this.head.getNext();
+		Node prev = this.head;
+		
+		// traverse pointer to required node
+		while (curr.getNext() != null && curr.getKey() != key) {
+			prev = curr;
+			curr = curr.getNext();
+		}
+		
+		if(curr.getKey() == key) {
+			prev.setNext(curr.getNext());
+			curr.setNext(null);
+		}
 		
 	}
-	
-	public void removeNext(Node node) {
-		if(node != null) {
-			node.setNext(node.getNext().getNext());
-			node.getNext().setNext(null);	
-		}
-	}
-	
-	public void removeKey(int key) {
-		Node temp = this.head;
-		while(temp.getNext() != null) {
-			if(temp.getNext().getKey() != key) {
-				temp = temp.getNext();	
-			}
-			else {
-				Node deleted = temp.getNext();
-				temp.setNext(deleted.getNext());
-				deleted.setNext(null);
-				break;
-			}
-		}
-	}
-	
+
 	public void removeDupWithoutAdditionalDS() {
 		Node p1 = head.getNext();
-		while(p1.getNext() != null) {
+		while (p1.getNext() != null) {
 			Node p2 = p1;
-			while(p2.getNext() != null) {
-				if(p2.getNext().getKey() == p1.getKey()) {
+			while (p2.getNext() != null) {
+				if (p2.getNext().getKey() == p1.getKey()) {
 					p2.setNext(p2.getNext().getNext());
-				}
-				else {
+				} else {
 					p2 = p2.getNext();
 				}
 			}
 			p1 = p1.getNext();
 		}
 	}
-	
+
 	public void removeDuplicates() {
 		Node p1 = head.getNext().getNext();
 		Node p2 = head.getNext();
 		Set<Integer> set = new HashSet<>();
 		set.add(p2.getKey());
-		
-		while(p1 != null) {
-			if(!set.contains(p1.getKey())) {
+
+		while (p1 != null) {
+			if (!set.contains(p1.getKey())) {
 				set.add(p1.getKey());
 				p1 = p1.getNext();
 				p2 = p2.getNext();
-			}
-			else {
+			} else {
 				Node deleted = p1;
 				p1 = p1.getNext();
 				p2.setNext(p1);
@@ -84,18 +90,18 @@ public class LinkedList {
 			}
 		}
 	}
-	
+
 	public void kthToLastNode(Integer k) {
 		kthtoLastNode(this.head, 3);
 	}
-	
+
 	private void kthtoLastNode(Node node, Integer k) {
-		if(node == null) {
+		if (node == null) {
 			this.count++;
 			return;
 		}
 		kthtoLastNode(node.getNext(), k);
-		if(count == k) {
+		if (count == k) {
 			System.out.println("Kth node is " + node.getKey() + " where k is " + k);
 		}
 		count++;
@@ -105,9 +111,9 @@ public class LinkedList {
 	public void removeMiddle() {
 		removeMiddle(this.head.getNext());
 	}
-	
+
 	private void removeMiddle(Node node) {
-		if(node.getNext() == null) {
+		if (node.getNext() == null) {
 			this.total++;
 			this.target = (int) Math.ceil(this.total / 2);
 			return;
@@ -115,22 +121,22 @@ public class LinkedList {
 		this.total++;
 		removeMiddle(node.getNext());
 		this.count++;
-		if(this.total - this.count == this.target - 1) {
+		if (this.total - this.count == this.target - 1) {
 			node.setNext(node.getNext().getNext());
 		}
 	}
-	
+
 	public void pivotAroundX(int x) {
 		// find pivot node
 		Node p1 = this.head.getNext();
-		while(p1.getKey() < x) {
+		while (p1.getKey() < x) {
 			p1 = p1.getNext();
 		}
-		
-		if(p1.getNext() != null) {
+
+		if (p1.getNext() != null) {
 			Node p2 = p1;
-			while(p1 != null) {
-				if(p1.getKey() < p2.getKey()) {
+			while (p1 != null) {
+				if (p1.getKey() < p2.getKey()) {
 					int temp = p1.getKey();
 					p1.setKey(p2.getKey());
 					p2.setKey(temp);
@@ -140,25 +146,25 @@ public class LinkedList {
 			}
 		}
 	}
-	
+
 	public void isLinkedListAPalindrome() {
 		isLinkedListPalindrome(this.head.getNext(), this.head.getNext());
 	}
-	
+
 	private void isLinkedListPalindrome(Node end, Node start) {
-		if(end == null) {
+		if (end == null) {
 			return;
 		}
 		isLinkedListPalindrome(end.getNext(), start);
-		if(this.isPalindrome && end.getKey() != start.getKey()) {
+		if (this.isPalindrome && end.getKey() != start.getKey()) {
 			this.isPalindrome = false;
 		}
 		start = start.getNext();
 	}
-	
-	public Node reverseLL(Node node) {
-		if(node.getNext() == null) {
-			Node temp = new Node(Integer.MIN_VALUE, node);
+
+	private Node reverseLL(Node node) {
+		if (node.getNext() == null) {
+			Node temp = node;
 			return temp;
 		}
 		Node temp = reverseLL(node.getNext());
@@ -167,50 +173,46 @@ public class LinkedList {
 		return temp;
 	}
 	
-	public void print() {
-		Node temp = head;
-		while(temp.getNext() != null) {
-			System.out.print(temp.getNext().getKey() + "==>");
-			temp = temp.getNext();
-		}
-		System.out.print("NULL\n");
+	public  void reverseLL() {
+		this.head = this.reverseLL(this.head);
 	}
-	
-	public void print(Node node) {
-		while(node.getNext() != null) {
-			System.out.print(node.getNext().getKey() + "==>");
+
+	private void print(Node node) {
+		while (node != null) {
+			System.out.print(node.getKey() + "==>");
 			node = node.getNext();
 		}
 		System.out.print("NULL\n");
 	}
 	
+	public void print() {
+		this.print(this.head);
+	}
+
 	public Node getHead() {
 		return this.head;
 	}
-	
+
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.add(2);
 		list.add(4);
 		list.add(6);
 		list.add(4);
-		list.add(4);
-		list.add(4);
 		list.add(10);
-		list.add(2);
-		list.add(2);
-		list.print(list.reverseLL(list.getHead()));
-		
-		
+		list.print();
+		//list.remove(6);
+		list.reverseLL();
+		list.print();
 	}
-	
+
 }
 
 class Node {
-	
+
 	private int key;
 	private Node next;
-	
+
 	public Node(int key, Node node) {
 		this.setKey(key);
 		this.setNext(node);
@@ -231,6 +233,5 @@ class Node {
 	public void setNext(Node next) {
 		this.next = next;
 	}
-	
-	
+
 }
