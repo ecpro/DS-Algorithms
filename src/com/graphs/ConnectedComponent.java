67dc9ffb70db82c9c4cb003eea1659f6e9d9de94
Piyush Ravi <mails4ipiyush@gmail.com>
2018-01-26 09:12:26 +0530

    connected component on acyclic graph

---
+++ src/com/graphs/ConnectedComponent.java
@@ -0,0 +1,49 @@
+package com.graphs;
+
+import java.util.Arrays;
+
+/**
+ * Created by eccspro on 22/01/18.
+ */
+public class ConnectedComponent {
+    private final boolean [] visited;
+    private final int [] id;
+    private int count = 0;
+
+    public ConnectedComponent(Graph g) {
+        visited = new boolean[g.getNumVertices()];
+        id = new int[g.getNumVertices()];
+        for(int v = 0; v < visited.length; v++) {
+            if(!visited[v]) {
+                bfs(g, v);
+                this.count++;
+            }
+        }
+    }
+
+    private void bfs(Graph g, int v) {
+        visited[v] = true;
+        id[v] = this.count;
+        for(int adjV : g.adjVertices(v)) {
+            if(!visited[adjV]) {
+                this.id[adjV] = this.count;
+                bfs(g, adjV);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < count; i++) {
+            sb.append("Connected component " + i + " [ ");
+            for(int v = 0; v < id.length; v++) {
+                if(id[v] == i) {
+                    sb.append(v + " ");
+                }
+            }
+            sb.append("]\n");
+        }
+        return sb.toString();
+    }
+}
