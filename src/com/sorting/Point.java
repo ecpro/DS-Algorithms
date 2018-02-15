1848b01788612fe04008d3b5e65d37309e7bd53a
Piyush Ravi <mails4ipiyush@gmail.com>
2018-02-15 08:13:27 +0530

    Point class complete implementation

--- src/com/sorting/Point.java
+++ src/com/sorting/Point.java
@@ -1,6 +1,9 @@
 package com.sorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public final class Point implements Comparable<Point>{
 	
@@ -29,15 +32,38 @@ public final class Point implements Comparable<Point>{
 	
 	public Comparator<Point> slopeOrder() {
 		Point p = this;
-		return new Comparator<Point>() {
-
-			@Override
-			public int compare(Point p1, Point p2) {
-				double slopeToP1 = p.slopeTo(p1);
-				double slopeToP2 = p.slopeTo(p2);
-				
-			}
-		};
+		return (p1, p2) -> {
+            double slopeToP1 = p.slopeTo(p1);
+            double slopeToP2 = p.slopeTo(p2);
+            return (int)(slopeToP1 - slopeToP2);
+        };
 	}
 
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public static void main(String [] args) {
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(3,1);
+        Point p3 = new Point(4,4);
+        Point p4 = new Point(-2,-5);
+        Point p5 = new Point(1,5);
+        Point p6 = new Point(1,-5);
+        List<Point> points = new ArrayList<>();
+        //points.add(p1);
+        points.add(p2);
+        points.add(p3);
+        points.add(p4);
+        points.add(p5);
+        points.add(p6);
+
+        Collections.sort(points, p1.slopeOrder());
+
+        System.out.println(points);
+    }
 }
